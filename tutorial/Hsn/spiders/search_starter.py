93720a05e5cd84a13acaa7e98bdfafebaf2c5357
yeong jinzhi <yeong_jz@hotmail.com>
2018-04-03 02:58:31 +0800

    adding various sorting functions

--- tutorial/Hsn/spiders/search_starter.py
+++ tutorial/Hsn/spiders/search_starter.py
@@ -6,7 +6,7 @@ import os.path
 import json
 import re
 from senticnet.senticnet import SenticNet
-from whoosh import index
+from whoosh import index, sorting
 from whoosh import highlight
 from whoosh.spelling import Corrector, QueryCorrector
 from whoosh.fields import ID, TEXT, Schema, NUMERIC
@@ -37,6 +37,9 @@ def is_number(s):
         return True
     except ValueError:
         return False
+
+def average(a, b):
+    return (a + b) / 2.0
 
 # get or initialise index
 def get_or_create_index(index_dir):
@@ -81,58 +84,14 @@ def full_index(index_dir):
             # convert to float type for sorting
             item_num_reviews = float(item["noOfReviews"][0:2])
         else:
-            # give arbitrary number if no price was stated on website
-            item_num_reviews = 100000
+            # give zero if null
+            item_num_reviews = 0
         if is_number(item["rating"][0:2].strip()):
             # convert to float type for sorting
             item_rating = float(item["rating"][0:2])
         else:
-            # give arbitrary number if no price was stated on website
-            item_rating = 100000
-        # check if string contains reviews
-        if len(item["review"]) == 0:
-            # for no reviews, assign a neutral score
-            averageIntensity = 0
-        else:
-            # initialise review polarity variable to store value of each review
-            intensityValueReview = 0
-            # iterate through each review in the list
-            for i in item["review"]:
-                # initialise word polarity variable to store value of each word
-                intensityValueWord = 0
-                # iterate through each word by using split otherwise will iterate by letter
-                for j in i.split():
-                    # catch exception when the word is not found in sentic net
-                    try:
-                        # add value of all the words
-                        intensityValueWord += float(sn.polarity_intense(j))
-                    except KeyError:
-                        # if not found in sentic net assign neutral value
-                        intensityValueWord += 0
-                # add the value of all the words to get the overall review score
-                intensityValueReview += intensityValueWord
-            # divide the total score of the reviews added together by the number of reviews to get an average value
-            averageIntensity = intensityValueReview/len(item["review"])                
-        writer.add_document(filename=item["name"], content=json.dumps(item), price=item_price, rating=item_rating, noOfReviews=item_num_reviews, review=item["review"], reviewPolarity=averageIntensity)
-    for item in data1:
-        if is_number(item["price"][1:].strip()):
-            # convert to float type for sorting
-            item_price = float(item["price"][1:])
-        else:
-            # give arbitrary number if no price was stated on website
-            item_price = 100000
-        if is_number(item["noOfReviews"][0:2].strip()):
-            # convert to float type for sorting
-            item_num_reviews = float(item["noOfReviews"][0:2])
-        else:
-            # give arbitrary number if no price was stated on website
-            item_num_reviews = 100000
-        if is_number(item["rating"][0:2].strip()):
-            # convert to float type for sorting
-            item_rating = float(item["rating"][0:2])
-        else:
-            # give arbitrary number if no price was stated on website
-            item_rating = 100000
+            # give 0 if no rating was stated on website
+            item_rating = 0
         # check if string contains reviews
         if len(item["review"]) == 0:
             # for no reviews, assign a neutral score
@@ -157,7 +116,53 @@ def full_index(index_dir):
                 intensityValueReview += intensityValueWord
             # divide the total score of the reviews added together by the number of reviews to get an average value
             averageIntensity = intensityValueReview/len(item["review"])
-        writer.add_document(filename=item["name"], content=json.dumps(item), price=item_price, rating=item_rating, noOfReviews=item_num_reviews, review=item["review"], reviewPolarity=averageIntensity)
+        if item_price != 100000:
+            writer.add_document(filename=item["name"], content=json.dumps(item), price=item_price, rating=item_rating, noOfReviews=item_num_reviews, review=item["review"], reviewPolarity=averageIntensity)
+    for item in data1:
+        if is_number(item["price"][1:].strip()):
+            # convert to float type for sorting
+            item_price = float(item["price"][1:])
+        else:
+            # give arbitrary number if no price was stated on website
+            item_price = 100000
+        if is_number(item["noOfReviews"][0:2].strip()):
+            # convert to float type for sorting
+            item_num_reviews = float(item["noOfReviews"][0:2])
+        else:
+            # give 0 if no reviews
+            item_num_reviews = 0
+        if is_number(item["rating"][0:2].strip()):
+            # convert to float type for sorting
+            item_rating = float(item["rating"][0:2])
+        else:
+            # give 0 if no rating was stated on website
+            item_rating = 0
+        # check if string contains reviews
+        if len(item["review"]) == 0:
+            # for no reviews, assign a neutral score
+            averageIntensity = 0
+        else:
+            # initialise review polarity variable to store value of each review
+            intensityValueReview = 0
+            # iterate through each review in the list
+            for i in item["review"]:
+                # initialise word polarity variable to store value of each word
+                intensityValueWord = 0
+                # iterate through each word by using split otherwise will iterate by letter
+                for j in i.split():
+                    # catch exception when the word is not found in sentic net
+                    try:
+                        # add value of all the words
+                        intensityValueWord += float(sn.polarity_intense(j))
+                    except KeyError:
+                        # if not found in sentic net assign neutral value
+                        intensityValueWord += 0
+                # add the value of all the words to get the overall review score
+                intensityValueReview += intensityValueWord
+            # divide the total score of the reviews added together by the number of reviews to get an average value
+            averageIntensity = intensityValueReview/len(item["review"])
+        if item_price != 100000:
+            writer.add_document(filename=item["name"], content=json.dumps(item), price=item_price, rating=item_rating, noOfReviews=item_num_reviews, review=item["review"], reviewPolarity=averageIntensity)
     writer.commit()
     return idx
 
@@ -187,22 +192,76 @@ def search(user_query, index_dir):
             except IndexError:
                 print('you need to provide a query. assuming "chicken" query.')
                 search('chicken', 'index_dir')
-        # do search
-        try:
-            # default is to sort by price for now can add other means of sorting by other fields
-            results = searcher.search(query, limit=50, sortedby="price")
-        except TermNotFound:
-            results = []
+        # query user for sorting method
+        sortMethod = input("Search by :\n1. Price(ascending)\n2. Price(descending)\n3. Reviews\n4. Ratings\n5. Reviews & Ratings\n6. Price, Reviews & Ratings\n7. Search by price range\nPlease enter your selection(1-7): ")
+
+        while not re.match(r"[1-7]", sortMethod):
+            sortMethod = input("Search by :\n1. Price(ascending)\n2. Price(descending)\n3. Reviews\n4. Ratings\n5. Reviews & Ratings\n6. Price, Reviews & Ratings\n7. Search by price range\nPlease enter your selection(1-7): ")
+        # ascending order by price
+        if sortMethod == "1":
+            try:
+                results = searcher.search(query, limit=20, sortedby="price")
+            except TermNotFound:
+                results = []
+        # descending order by price
+        elif sortMethod == "2":
+            try:
+                results = searcher.search(query, limit=20, sortedby="price", reverse=True)
+            except TermNotFound:
+                results = []
+        # sort by review polarity value with highest values first
+        elif sortMethod == "3":
+            try:
+                results = searcher.search(query, limit=20, sortedby="reviewPolarity", reverse=True)
+            except TermNotFound:
+                results = []
+        # sort by rating with highest values first
+        elif sortMethod == "4":
+            try:
+                results = searcher.search(query, limit=20, sortedby="rating", reverse=True)
+            except TermNotFound:
+                results = []
+        # sort by highest review polarity value and highest ratings
+        elif sortMethod == "5":
+            try:
+                rP = sorting.FieldFacet("reviewPolarity", reverse=True)
+                r = sorting.FieldFacet("rating", reverse=True)
+                weightedValue = sorting.TranslateFacet(average, rP, r)
+                results = searcher.search(query, limit=20, sortedby=weightedValue)
+            except TermNotFound:
+                results = []
+        # sort by lowest price, highest review polarity value, highest ratings
+        elif sortMethod == "6":
+            try:
+                rP = sorting.FieldFacet("reviewPolarity", reverse=True)
+                r = sorting.FieldFacet("rating", reverse=True)
+                weightedValue = sorting.TranslateFacet(average, rP, r)
+                results = searcher.search(query, limit=20, sortedby=[weightedValue, "price"])
+            except TermNotFound:
+                results = []
+        # search by price range
+        elif sortMethod == "7":
+            try:
+                priceRangeLow = int(input("Enter the minimum price : "))
+                priceRangeHigh = int(input("Enter the maximum price : "))
+                numResultsDisplayed = int(input("Enter the number of records to be shown : "))
+                results = searcher.search(query, limit=None, sortedby="price")
+            except TermNotFound:
+                results = []
+        
 
         # print results
         print("{} files matched:".format(len(results)))
-        # convert the arbitrary number into a more informative string
-        for hit in results:
-            if hit["price"] == 100000:
-                price = "No price stated."
-                print("  * {}".format(hit['filename']) +"\n" +"Price : ${}".format(price) + " Polarity of reviews(higher is better) : {0:.2f}".format(hit["reviewPolarity"]))
-            else:
-                print("  * {}".format(hit['filename']) +"\n" +"Price : ${}".format(str(hit["price"]))+ " Polarity of reviews(higher is better) : {0:.2f}".format(hit["reviewPolarity"]))
+        # print individual records
+        if sortMethod == "7":
+            count = 0
+            for hit in results:
+                if priceRangeLow <= hit["price"] <= priceRangeHigh and count<numResultsDisplayed:
+                    count+=1
+                    print("Result {} : * {} *".format(count, hit['filename']) +"\n" +"Price : ${}".format(str(hit["price"]))+ "\n" + "Review score(higher is better) : {0:.2f}".format(hit["reviewPolarity"]) + "\n" + "Rating(higher is better) : {0:.2f}".format(hit["rating"]))
+        else:
+            for hit in results:
+                print(" * {} *".format(hit['filename']) +"\n" +"Price : ${}".format(str(hit["price"]))+ "\n" + "Review score(higher is better) : {0:.2f}".format(hit["reviewPolarity"]) + "\n" + "Rating(higher is better) : {0:.2f}".format(hit["rating"]))
 
 
 if __name__ == '__main__':
