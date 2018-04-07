c85e9760cb1e8cbd1b733ec4436ef8fe6afe6aa9
yeong jinzhi <yeong_jz@hotmail.com>
2018-04-08 00:05:53 +0800

    updated requirements and added the training data and model

---
+++ tutorial/Hsn/spiders/training_algo.py
@@ -0,0 +1,134 @@
+import itertools
+import os
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from sklearn.preprocessing import LabelBinarizer, LabelEncoder
+from sklearn.metrics import confusion_matrix
+
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.preprocessing import text, sequence
+from keras import utils
+
+print("You have TensorFlow version", tf.__version__)
+
+train = pd.read_csv("test.csv")
+
+train.head()
+
+train_size = int(len(train) * .8)
+print ("Train size: %d" % train_size)
+
+train_posts = train['Item Name'][:train_size]
+train_tags = train['Category'][:train_size]
+
+test_posts = train['Item Name'][train_size:]
+test_tags = train['Category'][train_size:]
+
+vocab_size = 1000
+tokenize = text.Tokenizer(num_words=vocab_size)
+tokenize.fit_on_texts(train_posts)
+x_train = tokenize.texts_to_matrix(train_posts)
+x_test = tokenize.texts_to_matrix(test_posts)
+
+encoder = LabelEncoder()
+encoder.fit(train_tags)
+y_train = encoder.transform(train_tags)
+y_test = encoder.transform(test_tags)
+
+num_classes = np.max(y_train) + 1
+y_train = utils.to_categorical(y_train, num_classes)
+y_test = utils.to_categorical(y_test, num_classes)
+
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+print('y_train shape:', y_train.shape)
+print('y_test shape:', y_test.shape)
+
+batch_size = 32
+epochs = 2
+
+model = Sequential()
+model.add(Dense(512, input_shape=(vocab_size,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_split=0.1)
+
+score = model.evaluate(x_test, y_test,
+                       batch_size=batch_size, verbose=1)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
+text_labels = encoder.classes_ 
+
+for i in range(10):
+    prediction = model.predict(np.array([x_test[i]]))
+    predicted_label = text_labels[np.argmax(prediction)]
+    print(test_posts.iloc[i][:50], "...")
+    print('Actual label:' + test_tags.iloc[i])
+    print("Predicted label: " + predicted_label + "\n")
+
+y_softmax = model.predict(x_test)
+
+y_test_1d = []
+y_pred_1d = []
+
+for i in range(len(y_test)):
+    probs = y_test[i]
+    index_arr = np.nonzero(probs)
+    one_hot_index = index_arr[0].item(0)
+    y_test_1d.append(one_hot_index)
+
+for i in range(0, len(y_softmax)):
+    probs = y_softmax[i]
+    predicted_index = np.argmax(probs)
+    y_pred_1d.append(predicted_index)
+
+def plot_confusion_matrix(cm, classes,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title, fontsize=30)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45, fontsize=22)
+    plt.yticks(tick_marks, classes, fontsize=22)
+
+    fmt = '.2f'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label', fontsize=25)
+    plt.xlabel('Predicted label', fontsize=25)
+
+cnf_matrix = confusion_matrix(y_test_1d, y_pred_1d)
+plt.figure(figsize=(24,20))
+plot_confusion_matrix(cnf_matrix, classes=text_labels, title="Confusion matrix")
+plt.show()
